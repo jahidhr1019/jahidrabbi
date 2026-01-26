b1fd348a5d8c617e29a5e2812e66dcf78033a943
jahidhr1019 <jahidhr1019@gmail.com>
2026-01-26 22:10:34 +0600

    Create sitemap.ts

---
+++ sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://jahidrabbi.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://jahidrabbi.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
